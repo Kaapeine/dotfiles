1ee20a60024085ab06ba898978e7a291b090adbd
Vathsa <msshrivathsa@gmail.com>
2022-07-01 19:48:41 +0530

    Updating...

--- setup_i3.sh
+++ setup_i3.sh
@@ -9,4 +9,11 @@ sudo chmod +s /usr/bin/light
 sudo apt install playerctl
 
 # Setup polybar
-
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+git clone --recursive https://github.com/polybar/polybar
+cd polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
